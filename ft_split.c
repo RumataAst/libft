d4108c04b020efd44ed4dd68bd76a1ebb867c649
Andrei Kretov <akretov@c2r8s4.42prague.com>
2023-11-11 17:01:49 +0100

    norminette + tests

--- ft_split.c
+++ ft_split.c
@@ -6,104 +6,99 @@
 /*   By: akretov <akretov@student.42prague.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/01 15:32:52 by akretov           #+#    #+#             */
-/*   Updated: 2023/11/05 15:52:23 by akretov          ###   ########.fr       */
+/*   Updated: 2023/11/11 16:55:30 by akretov          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	ft_numb_words(const char *s, char c)
+static char	**free_array(char **ptr, int i)
+{
+	while (i > 0)
+	{
+		i--;
+		free(ptr[i]);
+	}
+	free(ptr);
+	return (0);
+}
+
+static int	ft_count_words(char const *str, char c)
 {
 	int	i;
 	int	count;
 
 	i = 0;
 	count = 0;
-	while (s[i] == c)
-		i++;
-	while (s[i])
+	while (str[i] != '\0')
 	{
-		if (s[i] == c && s[i + 1] != c)
-			count++;
-		i++;
-	}
-	return (count + 1);
-}
-
-char	*ft_strndup(const char *s, size_t n)
-{
-	char	*dest;
-	size_t	i;
-
-	i = 0;
-	dest = malloc(sizeof(char) * (n + 1));
-	if (dest == NULL)
-		return (0);
-	while (s[i] && i < n)
-	{
-		dest[i] = s[i];
-		i++;
-	}
-	dest[i] = '\0';
-	return (dest);
-}
-
-char	**ft_split(const char *s, char c)
-{
-	int		i;
-	int		j;
-	int		k;
-	char	**result;
-
-	i = 0;
-	j = 0;
-	k = 0;
-	result = (char **) malloc((ft_numb_words(s, c) + 1) * sizeof(char *));
-	if (result == NULL)
-		return (0);
-	while (s[i])
-	{
-		if (s[i] != c)
-		{
-			j = i;
-			while (s[i] && s[i] != c)
-				i++;
-			result[k] = ft_strndup(s + j, i - j);
-			if (result[k] == NULL)
-			{
-				k = 0;
-				while (result[k])
-					free(result[k++]);
-				free(result);
-				return (0);
-			}
-			k++;
-		}
-		else
+		if (str[i] == c)
 			i++;
+		else
+		{
+			count++;
+			while (str[i] && str[i] != c)
+				i++;
+		}
 	}
-	result[k] = NULL;
-	return (result);
+	return (count);
 }
 
-// int main(void)
-// {
-//     char    string[] = "fudsh fdsh fds h fd";
-//     char    c = ' ';
-//     char    **result = ft_split(string, c);
-// 	int z = ft_numb_words(string,c);
-// 	int i = 0;
-//     if  (result)
-//     {
-//         while (i < z)
-// 	{
-// 		printf("Word: %s\n", result[i]);
-//         free(result[i]);
-// 		i++;
-// 	}
-//         free(result);
-//     }
-//     else
-//         printf("Memory allocation failed.");
-//     return (0);
-// }
+static char	*ft_putword(char *word, char const *s, int i, int word_len)
+{
+	int	j;
+
+	j = 0;
+	while (word_len > 0)
+	{
+		word[j] = s[i - word_len];
+		j++;
+		word_len--;
+	}
+	word[j] = '\0';
+	return (word);
+}
+
+static char	**ft_split_words(char const *s, char c, char **s2, int num_words)
+{
+	int	i;
+	int	word;
+	int	word_len;
+
+	i = 0;
+	word = 0;
+	word_len = 0;
+	while (word < num_words)
+	{
+		while (s[i] && s[i] == c)
+			i++;
+		while (s[i] && s[i] != c)
+		{
+			i++;
+			word_len++;
+		}
+		s2[word] = (char *)malloc(sizeof(char) * (word_len + 1));
+		if (!s2)
+			return (free_array(s2, word));
+		ft_putword(s2[word], s, i, word_len);
+		word_len = 0;
+		word++;
+	}
+	s2[word] = 0;
+	return (s2);
+}
+
+char	**ft_split(char const *s, char c)
+{
+	char			**s2;
+	unsigned int	num_words;
+
+	if (!s)
+		return (0);
+	num_words = ft_count_words(s, c);
+	s2 = (char **)malloc(sizeof(char *) * (num_words + 1));
+	if (!s2)
+		return (0);
+	s2 = ft_split_words(s, c, s2, num_words);
+	return (s2);
+}
